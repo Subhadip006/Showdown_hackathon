1745d4039d87044f271aaecd1e95d141cd5a0afb
Subhadip006 <subhadip58512@gmail.com>
2025-01-18 11:29:24 +0530

    sign in page

---
+++ app/(auth)/login/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Login from "@/app/components/loginpage"
+
+function page() {
+  return (
+    <div className='flex justify-center items-center pt-10'>
+        <Login />
+    </div>
+  )
+}
+
+export default page
